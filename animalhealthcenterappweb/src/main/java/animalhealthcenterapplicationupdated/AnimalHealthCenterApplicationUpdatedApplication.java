30ea62267adf6be595c2fdf6d62a458cf3b2f354
Aremu Benjamen <bendayo38@gmail.com>
2019-03-19 13:34:38 -0700

    adding a multi module maven project. closes #1

---
+++ animalhealthcenterappweb/src/main/java/animalhealthcenterapplicationupdated/AnimalHealthCenterApplicationUpdatedApplication.java
@@ -0,0 +1,13 @@
+package animalhealthcenterapplicationupdated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalHealthCenterApplicationUpdatedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalHealthCenterApplicationUpdatedApplication.class, args);
+    }
+
+}
