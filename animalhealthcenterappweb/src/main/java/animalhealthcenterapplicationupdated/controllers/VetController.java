0ba0fefb1fef52980f3d248c262e34121bdc160d
Aremu Benjamen <bendayo38@gmail.com>
2019-03-19 17:29:18 -0700

    added vets page. closes #5

---
+++ animalhealthcenterappweb/src/main/java/animalhealthcenterapplicationupdated/controllers/VetController.java
@@ -0,0 +1,20 @@
+package animalhealthcenterapplicationupdated.controllers;
+
+import animalhealthcenterapplicationupdated.services.VetService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+    @RequestMapping({"", "/", "/vets", "/vets.html"})
+    public String vets(Model model){
+        model.addAttribute("vets", vetService.findAll());
+        return "vets/index";
+    }
+}
